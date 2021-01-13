56553a005f465e13c819260158a3eaa74bb7f438
mikikiv <46366395+mikikiv@users.noreply.github.com>
2021-01-12 22:07:53 -0800

    Site redesign, less pages, more apps

--- assets/js/calc-app.js
+++ assets/js/calc-app.js
@@ -7,24 +7,39 @@ function doMath(choise) {
   switch (c) {
     case "1":
       r = n1 + n2;
+      s = "+";
+
       break;
     case "2":
       r = n1 - n2;
+      s = "-";
       break;
     case "3":
       r = n1 * n2;
+      s = "X";
       break;
     case "4":
       r = n1 / n2;
+      s = "/";
       break;
     default:
       break;
   }
   document.getElementById("answer").value = r;
+  document.getElementById("equation").innerHTML=(n1)+(s)+(n2)+"="+(r);
+  document.getElementById("asymbol").innerHTML=(s);
 }
 
 function cleary() {
   document.getElementById("num1").value = "";
   document.getElementById("num2").value = "";
   document.getElementById("answer").value = "";
+  document.getElementById("equation").innerHTML="";
+}
+
+
+
+
+function logTime(){
+  console.log(Date);
 }
---
+++ assets/js/expressions.js
@@ -0,0 +1,2 @@
+let e=/a/;
+e.test("abc");
