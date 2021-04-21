9874fcad48e96e79b8afd1ab2abe2566ac4251aa
mikikiv <46366395+mikikiv@users.noreply.github.com>
2021-04-20 21:16:34 -0700

    Move copy functionality to class/ID buttons

--- assets/js/Wrapper.js
+++ assets/js/Wrapper.js
@@ -1,41 +1,39 @@
-function copy(that){
-    var inp =document.createElement('input');
-    document.body.appendChild(inp)
-    inp.value =that.textContent
-    inp.select();
-    document.execCommand('copy',false);
-    inp.remove();
-    document.getElementById("output").classname='classname';
-    }
-
-
-
-
 function doMath(choise) {
-    var n1, n2, n3, r, c;
-    n1 = document.getElementById("url").value;
-    n2 = document.getElementById("className").value;
-    n3 = document.getElementById("idName").value;
+    var urlSelector, classSelector, idSelector, aSelector, c, copyText;
+    urlSelector = document.getElementById("url").value;
+    classSelector = document.getElementById("className").value;
+    idSelector = document.getElementById("idName").value;
 
     c = choise;
 
-  
+ // Switch for selecting ID or Class input
 switch (c) {
     case "1":
-        r = "&target=class&class="+n2;
+        aSelector = "&target=class&class="+classSelector;
     break;
     case "2":
-        r = "&target=id&id="+n3;
+        aSelector = "&target=id&id="+idSelector;
     break;
     default:
     break;
     }
-    document.getElementById("output").innerHTML="https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/wrapper?site="+(n1)+(r)+"&title=Search&h1Ignore=Y";
+    //displays wrapper endpoint url
+    document.getElementById("output").value="https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/wrapper?site="+(urlSelector)+(aSelector)+"&title=Search&h1Ignore=Y";
+
+    //copies endpoint url when selecting ID or Class button
+    copyText = document.getElementById("output"); 
+    copyText.select();
+    document.execCommand("copy");
+
+    //popup alert
+    alert("Copied "+copyText.value+" to clipboard");
+
   }
   
+  //Clears out the page values
   function cleary() {
     document.getElementById("url").value = "";
     document.getElementById("className").value = "";
     document.getElementById("idName").value = "";
-    document.getElementById("output").innerHTML = "";
+    document.getElementById("output").value = "";
   }
