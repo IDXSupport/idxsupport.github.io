dc33da870cd16c0a47ba86424dd9a802661101b5
mikikiv <46366395+mikikiv@users.noreply.github.com>
2021-04-20 21:46:24 -0700

    Update CSS and toast design

--- assets/js/Wrapper.js
+++ assets/js/Wrapper.js
@@ -1,39 +1,48 @@
 function doMath(choise) {
-    var urlSelector, classSelector, idSelector, aSelector, c, copyText;
-    urlSelector = document.getElementById("url").value;
-    classSelector = document.getElementById("className").value;
-    idSelector = document.getElementById("idName").value;
+  var urlSelector, classSelector, idSelector, aSelector, c, copyText, toaster;
+  urlSelector = document.getElementById("url").value;
+  classSelector = document.getElementById("className").value;
+  idSelector = document.getElementById("idName").value;
 
-    c = choise;
+  c = choise;
 
- // Switch for selecting ID or Class input
-switch (c) {
+  // Switch for selecting ID or Class input
+  switch (c) {
     case "1":
-        aSelector = "&target=class&class="+classSelector;
-    break;
+      aSelector = "&target=class&class=" + classSelector;
+      break;
     case "2":
-        aSelector = "&target=id&id="+idSelector;
-    break;
+      aSelector = "&target=id&id=" + idSelector;
+      break;
     default:
-    break;
-    }
-    //displays wrapper endpoint url
-    document.getElementById("output").value="https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/wrapper?site="+(urlSelector)+(aSelector)+"&title=Search&h1Ignore=Y";
-
-    //copies endpoint url when selecting ID or Class button
-    copyText = document.getElementById("output"); 
-    copyText.select();
-    document.execCommand("copy");
-
-    //popup alert
-    alert("Copied "+copyText.value+" to clipboard");
-
+      break;
   }
-  
-  //Clears out the page values
-  function cleary() {
-    document.getElementById("url").value = "";
-    document.getElementById("className").value = "";
-    document.getElementById("idName").value = "";
-    document.getElementById("output").value = "";
-  }
+  //displays wrapper endpoint url
+  document.getElementById("output").value =
+    "https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/wrapper?site=" +
+    urlSelector +
+    aSelector +
+    "&title=Search&h1Ignore=Y";
+
+  //copies endpoint url when selecting ID or Class button
+  copyText = document.getElementById("output");
+  copyText.select();
+  document.execCommand("copy");
+
+  //popup alert unhidden with CSS
+  document.getElementById("copyUrl").innerHTML = output.value;
+  toaster = document.getElementById("toast");
+  toaster.className = "show";
+  setTimeout(function () {
+    toaster.className = toast.className.replace("show", "");
+  }, 3000);
+
+}
+
+//Clears out the page values
+function cleary() {
+  document.getElementById("url").value = "";
+  document.getElementById("className").value = "";
+  document.getElementById("idName").value = "";
+  document.getElementById("output").value = "";
+}
