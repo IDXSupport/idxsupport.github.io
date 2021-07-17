027caa750a9c9b93ebfb0c10437521d3bf635cf2
lainvineyard <lainvineyard@gmail.com>
2021-07-17 12:43:36 -0700

    added score function example

--- page-analyzer.js
+++ page-analyzer.js
@@ -1,29 +1,43 @@
 const puppeteer = require('puppeteer');
 
 /**
- * @param {*} url, the url we want to analyze to determine its wrapper capability
- * @returns A collection of data for wrapper analysis.
+ * An element that might be able to be used as a wrapper.
  */
-async function analyzeUrl(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+class WrapperCandidate {
+    /**
+     * 
+     * @param {*} type, whether this candidate is an 'id' or a 'class' 
+     * @param {*} name, the name of the id or class
+     * @param {*} element, the dom element associated with this candidate
+     */
+    constructor(type, name, element) {
+        this.type = type;
+        this.name = name;
+        this.element = element;
+        this.score = 0;
+    }
+}
 
-    // Setup console out
-    page.on('console', consoleMessage => console.log(consoleMessage.text()));
+/**
+ * @param {*} url, the url we want to analyze to determine its wrapper capability
+ * @returns An object collection of wrapper canidates for consideration. .classes for classes, .ids for ids. 
+ */
+async function analyzePage(page) {
 
     let out = {};
 
-    let classesMap = await page.evaluate(() => {
+    /**
+     * A map of class => number of occurrences
+     */
+    let candidateMapsJson = await page.evaluate(() => {
+
+        // Go through all the elements and see which IDs and classes might work...
         let elements = document.querySelectorAll('*');
 
-
         let classes = new Map();
+        let ids = new Map();
 
-        /**
-         * A map of class => number of occurrences 
-         */
-
+        // Count up how many of each class and id we have.
         for (let i=0; i<elements.length; i++) {
             let element = elements[i];
 
@@ -35,6 +49,8 @@ async function analyzeUrl(url) {
                 classes.has(name) ? classes.set(name, classes.get(name) + 1) : classes.set(name, 1);
             })
 
+            ids.has(element.id) ? ids.set(element.id, ids.get(element.id) + 1) : ids.set(element.id, 1);
+
         }
 
         /*
@@ -42,21 +58,39 @@ async function analyzeUrl(url) {
         console.log(elements.length);
         console.log(JSON.stringify(Array.from(classes.entries())));
         */
+       // Result has to be serializable, so let's keep it simple with an object.
 
-        return JSON.stringify(Array.from(classes.entries()));
+
+        return JSON.stringify({ 
+            classes: Array.from(classes.entries()),
+            ids: Array.from(ids.entries())
+        });
     });
 
-    // console.log(classesMap);
-    out.classes = new Map(JSON.parse(classesMap));
+    let candidateMaps = JSON.parse(candidateMapsJson);
 
-    await browser.close();
+    // console.log(classesMap);
+    out.classes = new Map(candidateMaps.classes);
+    out.ids = new Map(candidateMaps.ids);
+
     return out;
 }
 
 let url = 'https://duckduckgo.com/';
 
 (async () => {
-    let analysis = await analyzeUrl(url);
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // Setup console out
+    page.on('console', consoleMessage => console.log(consoleMessage.text()));
+    page.on('pageerror', err => console.log(err));
+
+    await page.goto(url);
+
+    let analysis = await analyzePage(page);
+
+    let candidates = [];
 
     console.log(`Classes that have occurred exactly once on ${url}`);
 
@@ -65,9 +99,120 @@ let url = 'https://duckduckgo.com/';
     analysis.classes.forEach((value, key) => {
         if (value == 1) {
             out += `${key}, `;
+            candidates.push(new WrapperCandidate('class', key));
         }
     });
 
     console.log(out);
+    console.log("");
+    console.log(`Ids that have occurred exactly once on ${url}`);
+
+    let idsOut = '';
+    analysis.ids.forEach((value, key) => {
+        if (value==1) {
+            idsOut += `${key}, `;
+            candidates.push(new WrapperCandidate('id', key));
+        } 
+    });
+
+    console.log(idsOut);
+
+    console.log("");
+    console.log("============================");
+    console.log("");
+
+    // Score each candidate
+    await page.evaluate((candidates) => {
+                
+        // Scores a wrapper candidate against a provided HTML document.
+        // TODO move this out of the page evaulate block
+        function simpleScore(candidate, document) {
+            let type = candidate.type;
+            let element;
+
+            if (!document) {
+                throw 'No document provided to score against';
+            }
+
+            if (type == 'class') {
+                element = document.getElementsByClassName(candidate.name)[0];
+            } else if (type == 'id'){
+                element = document.getElementById(candidate.name);
+            }
+
+            if (!element) {
+                throw "Scorer could not find the element indicated by the candidate"; 
+            }
+
+
+            // Start the scoring
+            let score = 0;
+
+
+            let boundingRectangle = element.getBoundingClientRect();
+            
+            // We should move these into an array of functions that are evaluated for t/f and increase / decrease the score accordingly
+            
+            if (boundingRectangle.x != 0) {
+                // Further away from the left is better??
+                score += 1;
+            }
+
+            if (boundingRectangle.y !=   0) {
+                // Lower on the page is better??
+                score += 1;
+            }
+
+            if (boundingRectangle.y < 1000) {
+                // But being close to the top of the page is good too.
+                score += 2 - Math.abs(100-boundingRectangle.y)/1000;
+            }
+
+            if (boundingRectangle.width > 300) {
+                // Wider is better??
+                score += 1;
+            }
+
+            if (boundingRectangle.height > 300) {
+                // Taller is better??
+                score += 1;
+            }
+
+            // Get a rough count of th      e number of words in the element and increase the score based on how many this element contains
+            let text = element.textContent;
+            score += text.split(" ").length / 10000;
+
+            candidate.score = score;
+        }
+
+        console.log(candidates.length);
+        candidates.forEach((candidate) => {
+            let score = 0;
+
+            simpleScore(candidate, document);
+
+            // console.log(`${candidate.type} ${candidate.name}: ${candidate.score}`);
+
+
+            candidates.sort((a, b) => {
+                return b.score - a.score;
+            });
+
+
+        });
+
+        console.log("Sorted candidates:");
+        console.log("");
+        let count = 0;
+        for (let candidate of candidates) {
+            console.log(`${candidate.type} ${candidate.name}: ${candidate.score}`);
+            count++;
+        }
+    
+
+    }, candidates);
+
+    await browser.close();
+
 })();
 
