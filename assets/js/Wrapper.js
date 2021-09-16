835e48ea61815f127e4b3dd02f127e1eaf0f3de0
lainvineyard <lainvineyard@gmail.com>
2021-09-15 18:15:13 -0700

    make the endpoint creator trim whitespace and replace spaces with commas from the target input

--- assets/js/Wrapper.js
+++ assets/js/Wrapper.js
@@ -17,14 +17,22 @@ function copy() {
   toasterNoti();
 }
 
-function doMath(choise) {
+// Removes any surrounding whitespace and Replaces any spaces with commas for use with the wrapper endpoint.
+function formatTarget(target) {
+  return target.trim().replace(' ', ',');
+}
+
+function doMath(choice) {
   //This is the function that adds the query to the URL and the class or ID name. Assigns a value
   //to these vars when any button is clicked
   var urlSelector, elementSelector, totalSelector, c;
   urlSelector = document.getElementById("url").value; //take the value from the URL field
-  elementSelector = document.getElementById("idName").value; //take the value from the class/id/element field 
+  elementSelector = formatTarget(document.getElementById("idName").value); //take the value from the class/id/element field 
 
-  c = choise;
+  // Might have changed the actual target with formatTarget, so set the idName input to whatever elementSelector is now.
+  document.getElementById("idName").value = elementSelector;
+
+  c = choice;
 
   // switch call for selecting ID or Class input
   switch (c) {
