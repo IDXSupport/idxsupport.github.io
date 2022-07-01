6c3adba37f0babb6ac0fed2ce79c6ab92eb52918
Mikey <46366395+mikikiv@users.noreply.github.com>
2022-06-30 23:22:37 -0700

    Error handling & replace deprctd copy action

--- assets/js/Wrapper.js
+++ assets/js/Wrapper.js
@@ -1,12 +1,20 @@
 
-function toasterNoti(displayText) {
+function toasterNoti(isTrue,displayText) {
   //popup alert unhidden with CSS
   document.getElementById("copyUrl").innerHTML = displayText;
   toaster = document.getElementById("toast");
-  toaster.className = "show";
-  setTimeout(function () {
-    toaster.className = toast.className.replace("show", "");
-  }, 5000);
+  // toaster.className = "show";
+  if (isTrue == true) {
+    toaster.className = "success show"
+    setTimeout(function () {
+      toaster.className = toast.className.replace("success show", "");
+    }, 5000);
+  } else {
+    toaster.className = "fail show"
+    setTimeout(function () {
+      toaster.className = toast.className.replace("fail show", "");
+    }, 5000);
+  }
 }
 
 function copyNew() {
@@ -14,7 +22,7 @@ function copyNew() {
   //display toast notification
   copyText = document.getElementById("output");
   copyText.select();
-  document.execCommand("copy");
+  navigator.clipboard.writeText(copyText.value);
 }
 
 // Removes any surrounding whitespace and Replaces any spaces with commas for use with the wrapper endpoint.
@@ -37,7 +45,7 @@ function displayStorage(idName) {
   
   copyButton.onclick = function(){
     copyDivToClipboard(idName)
-    toasterNoti(label.innerHTML)
+    toasterNoti(true,label.innerHTML)
   };
   document.getElementById("list").prepend(li)
   document.getElementById(idName).prepend(label)
@@ -50,7 +58,7 @@ function copyDivToClipboard(elementId) {
   console.log(elementId)
   window.getSelection().removeAllRanges(); // clear current selection
   window.getSelection().addRange(range); // to select text
-  document.execCommand("copy");
+  navigator.clipboard.writeText(elementId.value);
   window.getSelection().removeAllRanges();// to deselect
 }
 
@@ -63,68 +71,77 @@ function doMath(choice) {
 
   // Might have changed the actual target with formatTarget, so set the idName input to whatever elementSelector is now.
   document.getElementById("idName").value = elementSelector;
-
-  if(document.getElementById("title").value != ''){
-    title = "&title=" + document.getElementById("title").value;
+  
+  if (urlSelector == '') {
+    toasterNoti(false, "You should probably enter a URL")
+  } else if (isValidHttpUrl(urlSelector) != true) {
+    toasterNoti(false, "Your URL doesn't look right... Did you include the protocal?")
   } else {
-    title = '';
+    if (elementSelector == '') {
+      toasterNoti(false, "You're going to need to choose an element")
+    } else {
+      if (document.getElementById("title").value != '') {
+        title = "&title=" + document.getElementById("title").value;
+      } else {
+        title = '';
+      }
+
+
+    if (document.getElementById("h1ignoreCheck").checked == 1) {
+      h1yn = "&h1Ignore=Y";
+    } else {
+      h1yn = "&h1Ignore=N";
+    };
+
+
+    c = choice;
+
+    // switch call for selecting ID or Class input
+    switch (c) {
+      case "class":
+        urlSelector = "wrapper-v2?site=" + urlSelector;
+        totalSelector = "&target=class&class=" + elementSelector;
+        break;
+      case "id":
+        urlSelector = "wrapper-v2?site=" + urlSelector;
+        totalSelector = "&target=id&id=" + elementSelector;
+        break;
+      case "duda":
+        urlSelector = "duda?site=" + urlSelector;
+        totalSelector = "&target=id&id=" + elementSelector;
+        break;
+      case "element":
+        urlSelector = "wrapper-v2?site=" + urlSelector;
+        totalSelector = "&target=element&el=" + elementSelector;
+        break;
+      default:
+        break;
+    }
+
+    const outPut = document.getElementById("output")
+    
+    
+    
+    //displays wrapper endpoint url
+    outPut.value =
+      "https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/" +
+      urlSelector +
+      totalSelector +
+      title +
+      h1yn;
+    copyNew();
+
+    let text = document.getElementById("url")
+    toasterNoti(true, c.toUpperCase() + ' selected for "' + text.value + '"')
+    // Add the data to local storage for react to pull
+    localStorage.setItem(storageValue, outPut.value)
+
+
+    
+    displayStorage(storageValue);
+    storageValue++
   }
-
-
-  if (document.getElementById("h1ignoreCheck").checked == 1) {
-    h1yn = "&h1Ignore=Y";
-  } else {
-    h1yn = "&h1Ignore=N";
-  };
-
-
-  c = choice;
-
-  // switch call for selecting ID or Class input
-  switch (c) {
-    case "class":
-      urlSelector = "wrapper-v2?site=" + urlSelector;
-      totalSelector = "&target=class&class=" + elementSelector;
-      break;
-    case "id":
-      urlSelector = "wrapper-v2?site=" + urlSelector;
-      totalSelector = "&target=id&id=" + elementSelector;
-      break;
-    case "duda":
-      urlSelector = "duda?site=" + urlSelector;
-      totalSelector = "&target=id&id=" + elementSelector;
-      break;
-    case "element":
-      urlSelector = "wrapper-v2?site=" + urlSelector;
-      totalSelector = "&target=element&el=" + elementSelector;
-      break;
-    default:
-      break;
   }
-
-  const outPut = document.getElementById("output")
-  
-  
-  
-  //displays wrapper endpoint url
-  outPut.value =
-  "https://zl6t6xxpc2.execute-api.us-west-2.amazonaws.com/wrappers/" +
-  urlSelector +
-  totalSelector +
-  title +
-  h1yn;
-  copyNew();
-
-  let text = document.getElementById("url")
-  toasterNoti(c + " ----- " + text.value )
-  // Add the data to local storage for react to pull
-  localStorage.setItem(storageValue, outPut.value)
-
-
-  
-  displayStorage(storageValue);
-  storageValue++
-  
 }
 
 function cleary() {
@@ -155,4 +172,17 @@ function loadStorage() {
 function copyFromStorage(target) {
   copyTarget = document.getElementById(target).innerHTML
   console.log(localStorage.getItem(target))
+}
+
+
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
 }
