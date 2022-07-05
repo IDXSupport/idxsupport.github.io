edeb5bc5a0b44d0c573332bb7791b29ed230de43
Mikey <46366395+mikikiv@users.noreply.github.com>
2022-07-04 22:59:57 -0700

    Rework to use classes

--- src/app.js
+++ src/app.js
@@ -1,50 +1,60 @@
-const siteTop = (
-    <div id="" className="analyzer-app">
-        <h1 className="analyzer-app-header">
-        <span className="header-first-part">Candi</span><span className="header-second-part">date</span> <span
-            className="header-third-part">Calculator</span>
-        </h1>
 
-        <div className="app-intranavigation">
-        <a className="intrasite current" href="https://idxsupport.github.io/">Endpoint Creator</a>
-        <a className="intrasite" href="http://lain.tools:5732/analyzer.html">Candidate Calculator</a>
-        </div>
-    </div>
-)
+class SiteTop extends React.Component {
+    render() {
+        return (
+            <div id="" className="analyzer-app">
+                <h1 className="analyzer-app-header">
+                    <span className="header-first-part">Candi</span>
+                    <span className="header-second-part">date</span>
+                    <span className="header-third-part">Calculator</span>
+                </h1>
+                <div className="app-intranavigation">
+                    <a className="intrasite current" href="https://idxsupport.github.io/">Endpoint Creator</a>
+                    <a className="intrasite" href="http://lain.tools:5732/analyzer.html">Candidate Calculator</a>
+                </div>
+            </div>
+        )
+    }
+}
+class InputLabels extends React.Component {
+    render() {
+        const inputFields = [
+            ['URL', 'http://www.example.com/'],
+            ['Target', 'Class(es)/ID/Element Name'],
+            ['Title', 'Search']
+        ];
+        const inputLabels = inputFields.map(inputFields =>
+            <div className = "row input-fields">
+                <label className="badge-dark">{inputFields[0]}</label>
+                <input
+                    id={inputFields[0].toLowerCase()}
+                    placeholder={inputFields[1]}
+                    defaultValue={inputFields[1] == 'Search' ? 'Search' : ''}
+                    type={inputFields[0] == 'url' ? 'url' : ''}
+                />
+            </div>
+        );
+        return <div> {inputLabels} </div>
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <SiteTop />
+                <section id="calculator">
+                    <div className="app calculator">
+                        <h1>Wrapper Endpoint Creators</h1>
+                        <InputLabels />
+                    </div>
 
-ReactDOM.render(siteTop,document.getElementById('topNav'))
-
-const sectionHeader = <h1>Wrapper Endpoint Creator</h1>;
-
-const app = (
-    <div>
-        {sectionHeader}
-        <div id="inputs"> </div>
-
-    </div>
-    )
- 
-
-
-const inputFields = [
-    ['Url', 'http://www.example.com/'],
-    ['Target', 'Class(es)/ID/Element Name'],
-    ['Title', 'Search']
-];
-
-const inputLabels = inputFields.map(inputFields =>
-    <div className = "row input-fields">
-        <label className="badge-dark">{inputFields[0]}</label>
-        <input
-            id={inputFields[0].toLowerCase()}
-            placeholder={inputFields[1]}
-            defaultValue={inputFields[1] == 'Search' ? 'Search' : ''}
-            type={inputFields[0] == 'url' ? 'url' : ''}
-        />
-    </div>
-);
-
+                </section>
+            </div>            
+        )
+    }
+}
 
 ReactDOM.render(
-    inputLabels,
-document.getElementById('appCalculator'));
+    <App />,
+    document.getElementById('root')
+)
